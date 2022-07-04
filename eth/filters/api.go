e8121f8d7a9a5a71f0ccd467f7158d38e0020b50
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-07-04 02:26:56 +0100

    Update api.go

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -162,7 +162,9 @@ func (api *PublicFilterAPI) NewPendingTransactions(ctx context.Context) (*rpc.Su
 				// To keep the original behaviour, send a single tx hash in one notification.
 				// TODO(rjl493456442) Send a batch of tx hashes in one notification
 				for _, h := range hashes {
-					notifier.Notify(rpcSub.ID, h)
+					result, data := api.byHash.GetTransactionByHash(ctx, h)
+					//data := h
+					notifier.Notify(rpcSub.ID, result)
 				}
 			case <-rpcSub.Err():
 				pendingTxSub.Unsubscribe()
