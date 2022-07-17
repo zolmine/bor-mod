263d51ee2a49e7800994ff70c22f0d3e7005282f
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-07-17 13:14:22 +0100

    Update api.go

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -636,9 +636,9 @@ func (api *PublicFilterAPI) NewPendingTransactionsComplite(ctx context.Context) 
 				for _, h := range hashes {
 					fmt.Printf("h: %s\n", reflect.TypeOf(h))
 					fmt.Print(h)
-					// resultsT, _ := api.client.GetTransactionByHash(ctx,h)
+					resultsT := api.client.GetTransactionByHash(ctx,h)
 					// fmt.Printf("h: %s\n", reflect.TypeOf(resultsT))
-					notifier.Notify(rpcSub.ID, [api.client.GetTransactionByHash(ctx,h)])
+					notifier.Notify(rpcSub.ID, resultsT)
 				}
 			case <-rpcSub.Err():
 				pendingTxSub.Unsubscribe()
