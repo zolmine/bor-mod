377004771bf8648df2ce6b40a6c80cc83d4af5ca
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-08-16 12:13:51 +0100

    Update api.go

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -201,7 +201,7 @@ func (api *PublicFilterAPI) SubscribeFullPendingTransactions(ctx context.Context
 				// To keep the original behaviour, send a single tx hash in one notification.
 				// TODO(rjl493456442) Send a batch of tx hashes in one notification
 				for _, tx := range txs {
-					fmt.Print(time)
+					fmt.Print(time.Unix())
 					from, err := types.Sender(types.NewEIP155Signer(tx.ChainId()), tx) 
 					if err != nil {
 						from, _ := types.Sender(types.HomesteadSigner{}, tx) 
