d87386e53164acebdab5ea1efa328b0f7b62dc76
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-08-19 13:54:49 +0100

    Update api.go

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -241,7 +241,7 @@ func (api *PublicFilterAPI) SubscribeFullPendingTransactions(ctx context.Context
 func itemExists(slice interface{}, item interface{}) bool {
 	s := reflect.ValueOf(slice)
 
-	fmt.Print(s.Index(0).Interface(),item,"\n")
+	fmt.Print(s.Index(0).Interface(),"\n")
 	
 	if s.Kind() != reflect.Slice {
 		panic("Invalid data-type")
