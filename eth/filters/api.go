abbf46442433c119abe753e4e01bf075e2347f8b
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-08-22 10:44:38 +0100

    Update api.go

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -62,7 +62,7 @@ type PublicFilterAPI struct {
 	filters   map[rpc.ID]*filter
 	timeout   time.Duration
 	borLogs   bool
-	data     []*ethapi.PublicTxPoolAPI
+	ethAPI    *ethapi.PublicBlockChainAPI
 	chainConfig *params.ChainConfig
 }
 
@@ -270,7 +270,7 @@ func (api *PublicFilterAPI) SubscribeFullPendingTransactions(ctx context.Context
 
 	go func() {
 		txs := make(chan []*types.Transaction, 128)
-		txsTime := api.data
+		txsTime := api.ethAPI
 		pendingTxSub := api.events.SubscribePendingTxs(txs)
 
 		fmt.Print(txsTime.Content())
