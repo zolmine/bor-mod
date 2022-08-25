7e7ed139fc6f8ff67a780f071502acc4d5c75786
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-08-25 13:35:00 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1653,9 +1653,9 @@ func (s *PublicTransactionPoolAPI) GetTransactionCount(ctx context.Context, addr
 }
 
 // GetTransactionByHash returns the transaction for the given hash
-func (s *PublicTransactionPoolAPI) GetTransactionByHash01(ctx context.Context, hash common.Hash) (*RPCTransaction, error) {
+func (s *PublicTransactionPoolAPI,m *PublicTxPoolAPI ) GetTransactionByHash01(ctx context.Context, hash common.Hash) (*RPCTransaction, error) {
 	borTx := false
-	pending, queue := *PublicTxPoolAPI.b.TxPoolContent()
+	pending, queue := m.b.TxPoolContent()
 
 	fmt.Println("this is all txs1: ", len(pending), "\n")
 	// Try to return an already finalized transaction
