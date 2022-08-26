dcb3a7da7d2ae52f405f83980c6a6d4401dff94a
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-08-26 15:17:19 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1674,7 +1674,7 @@ var (
 
 	add1, _ = decodeAddress("0xC36442b4a4522E871399CD717aBDD847Ab11FE88")
 	add2, _ = decodeAddress("0x2953399124F0cBB46d2CbACD8A89cF0599974963")
-	add3, _ := decodeAddress("0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45")
+	add3, _ = decodeAddress("0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45")
 	// inp1 = 
 )
 
