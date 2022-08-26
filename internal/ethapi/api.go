66be2565982d1af85da852e9dd03d0d57b557a44
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-08-26 14:36:22 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1672,12 +1672,12 @@ func (s *PublicTransactionPoolAPI) GetTransactionByHash01(ctx context.Context, h
 }
 
 func tree(tx *types.Transaction,currentGas *big.Int) *big.Int{
-	fmt.Print("fullTx: ", tx.GasPrice(), "\n")
-	// if *currentGas > *tx.GasPrice(){
-	// 	return tx.GasPrice()
-	// } else {
-	// 	return currentGas
-	// }
+	// fmt.Print("fullTx: ", tx.GasPrice(), "\n")
+	if currentGas.Cmp(tx.GasPrice()) == -1 {
+		return tx.GasPrice()
+	} else {
+		return currentGas
+	}
 	return currentGas
 }
 func (s *PublicTransactionPoolAPI) GetTransactionByHash(ctx context.Context, hash common.Hash) (*RPCTransaction, error) {
