300163b7b6adabacde12ee2e281c06efa1106ad3
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-05 13:08:38 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1148,7 +1148,7 @@ func (s *PublicBlockChainAPI) Call(ctx context.Context, args TransactionArgs, bl
 	}
 	return result.Return(), result.Err
 }
-func (s *PublicBlockChainAPI) CallForTest(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumber, overrides *StateOverride) (hexutil.Bytes,hexutil.Bytes, error) {
+func (s *PublicBlockChainAPI) CallForTest(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes,hexutil.Bytes, error) {
 	result, resultBefore, err := DoCallForTest(ctx, s.b, args, args0, blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
 	if err != nil {
 		return nil, err
