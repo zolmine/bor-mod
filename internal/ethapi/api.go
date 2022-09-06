b1929e59262f347a16fd4e5d557487af1bafee96
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-06 10:39:32 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1748,7 +1748,7 @@ func (s *PublicTransactionPoolAPI) GetTransactionByHash02(ctx context.Context, a
 	pending, _ := s.b.TxPoolContent()
 	
 	fmt.Println("this is all txs1: ", len(pending), "\n")
-	curentGas := big.NewInt(0)
+	// curentGas := big.NewInt(0)
 	// for _, txs := range pending {
 	// 	for _, tx := range txs {
 	// 		// fmt.Print("fullTx: ", tx.GasPrice(), "\n")
