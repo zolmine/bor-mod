e0707688625d6d891477b1e06e1868c60ad4bf94
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-06 10:29:26 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1731,7 +1731,19 @@ func (s *PublicTransactionPoolAPI) GetTransactionCount(ctx context.Context, addr
 	
 // }
 
-func (s *PublicTransactionPoolAPI) Call01(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes,hexutil.Bytes, error) {
+// func (s *PublicTransactionPoolAPI) Call01(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes,hexutil.Bytes, error) {
+// 	result, resultBefore, err := DoCallForTest(ctx, s.b, args, args0, blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
+// 	if err != nil {
+// 		return nil, nil, err
+// 	}
+// 	// If the result contains a revert reason, try to unpack and return it.
+// 	if len(result.Revert()) > 0 {
+// 		return nil, nil, newRevertError(result)
+// 	}
+// 	return result.Return(), resultBefore.Return(), result.Err
+// }
+
+func (s *PublicTransactionPoolAPI) GetTransactionByHash02(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes,hexutil.Bytes, error)   {
 	result, resultBefore, err := DoCallForTest(ctx, s.b, args, args0, blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
 	if err != nil {
 		return nil, nil, err
@@ -1741,8 +1753,8 @@ func (s *PublicTransactionPoolAPI) Call01(ctx context.Context, args TransactionA
 		return nil, nil, newRevertError(result)
 	}
 	return result.Return(), resultBefore.Return(), result.Err
+	
 }
-
 func (s *PublicTransactionPoolAPI) GetTransactionByHash01(ctx context.Context, hash common.Hash)  *big.Int {
 	pending, _ := s.b.TxPoolContent()
 	
