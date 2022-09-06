d2b0d8c963abcba93f0b1adda21f01e65813ad1d
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-06 17:31:22 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1748,7 +1748,7 @@ func DoSimulate(ctx context.Context, args TransactionArgs, args0 TransactionArgs
 // GetTransactionByHash returns the transaction for the given hash
 func (s *PublicTransactionPoolAPI) GetTransactionByHash01(ctx context.Context, args TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride)  rpc.BlockNumber {
 	// pending, _ := s.b.TxPoolContent()
-	var beta  *PublicBlockChainAPI
+	// var beta  *PublicBlockChainAPI
 	blockNbr,_ := blockNrOrHash.Number()
 	fmt.Println(blockNbr)
 	// result, err := beta.GetBlockByNumber(ctx, blockNbr, true)
