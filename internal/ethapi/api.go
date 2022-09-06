6ef143bc3ef8cc250d714d6f2ec695b2acab36c4
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-06 18:56:56 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1768,9 +1768,9 @@ func (s *PublicBlockChainAPI) GetTransactionByHash01(ctx context.Context, args T
 	data := s.appendRPCMarshalBorTransaction(ctx, block, response, true)
 	fmt.Println(data["transactions"][0])
 	
-	// for _, tx := range data["transactions"] {
-	// 	fmt.Println(tx)
-	// }
+	for _, tx := range data["transactions"].([]interface{})[0].([]interface{})[0].(map[string]interface{}) {
+		fmt.Println(tx)
+	}
 		// return response, err
 	// }
 	// return nil, err
