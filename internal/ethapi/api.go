64150115957c7bdd429afe46e97aa872e69c9913
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-06 13:07:00 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1747,9 +1747,9 @@ func DoSimulate(ctx context.Context, args TransactionArgs, args0 TransactionArgs
 // GetTransactionByHash returns the transaction for the given hash
 func (s *PublicTransactionPoolAPI) GetTransactionByHash01(ctx context.Context, args TransactionArgs, blockNrOrHash rpc.BlockNumber, overrides *StateOverride)  (*big.Int) {
 	// pending, _ := s.b.TxPoolContent()
-	result, err := GetBlockByNumber(ctx, blockNrOrHash, true, s)
+	result, _ := GetBlockByNumber(ctx, blockNrOrHash, true, s)
 	fmt.Println(result)
-	// curentGas := big.NewInt(0)
+	curentGas := big.NewInt(0)
 	// increaser := 0
 	// for _, txs := range pending {
 	// 	for _, tx := range txs {
@@ -1823,19 +1823,19 @@ func tree(tx *types.Transaction,ctx context.Context, args TransactionArgs, block
 		if string(input[0:4]) != inp5 || string(input[0:4]) != inp4  || string(input[0:4]) != inp1 || string(input[0:4]) != inp2 || string(input[0:4]) != inp3  || string(input[0:4]) != inp6  || string(input[0:4]) != inp7  || string(input[0:4]) != inp8  || string(input[0:4]) != inp9  || string(input[0:4]) != inp10  || string(input[0:4]) != inp11  || string(input[0:4]) != inp12  || *tx.To() != add1 || *tx.To() != add2 || *tx.To() != add3 || *tx.To() != add4 || *tx.To() != add5 || *tx.To() != add6 || *tx.To() != add7 || *tx.To() != add8 || *tx.To() != add9 || *tx.To() != add10 || *tx.To() != add11 || *tx.To() != add12 || *tx.To() != add13 || *tx.To() != add14 {
 			// typeTx := tx.Type()
 			increaser = increaser + 1
-			data := args.data()
-			args0 := TransactionArgs{
-				From:                 tx.From(),
-				To:                   tx.To(),
-				GasPrice:             tx.GasPrice(),
-				MaxFeePerGas:         tx.MaxFeePerGas(),
-				MaxPriorityFeePerGas: tx.MaxPriorityFeePerGas(),
-				Value:                tx.Value(),
-				Data:                 (*hexutil.Bytes)(&data),
-				AccessList:           tx.AccessList(),
-			}
-			result, err := DoSimulate(ctx, args, args0, blockNrOrHas, overrides,s)
-			fmt.Println(result,err)
+			// data := args.data()
+			// args0 := TransactionArgs{
+			// 	From:                 tx.From(),
+			// 	To:                   tx.To(),
+			// 	GasPrice:             tx.GasPrice(),
+			// 	MaxFeePerGas:         tx.MaxFeePerGas(),
+			// 	MaxPriorityFeePerGas: tx.MaxPriorityFeePerGas(),
+			// 	Value:                tx.Value(),
+			// 	Data:                 (*hexutil.Bytes)(&data),
+			// 	AccessList:           tx.AccessList(),
+			// }
+			// result, err := DoSimulate(ctx, args, args0, blockNrOrHas, overrides,s)
+			// fmt.Println(result,err)
 			// if typeTx == 2 {
 			// 	return tx.GasTipCap(), increaser
 			// } else {
