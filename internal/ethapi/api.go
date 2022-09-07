8d95533c864f6eb8def0c91444bd9d919fd60de2
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-07 09:55:45 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1771,7 +1771,7 @@ func (s *PublicBlockChainAPI) GetTransactionByHash01(ctx context.Context, args T
 	toVar := reflect.ValueOf(data["transactions"])
 	for i := 0; i < toVar.Len(); i++ {
 		singleVertex := toVar.Index(i).Elem() // What to do here?
-		fmt.Println(singleVertex.Index(0))
+		fmt.Println(singleVertex.FieldByName("hash"))
 	  }
 	// for _, tx := range data["transactions"].([]interface{}) {
 	// 	fmt.Println(tx)
