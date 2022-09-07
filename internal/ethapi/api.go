26ffd4d4a48dbb53fdd7d8773c735e925e0376de
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-09-07 22:10:23 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1772,19 +1772,7 @@ func DoCallForAllTest(ctx context.Context, b Backend, args TransactionArgs, bloc
 
 }
 
-func DoSimulate(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride, s *PublicTransactionPoolAPI) (hexutil.Bytes, error) {
-	resultBefore, err := DoCallForTest(ctx, s.b, args, args0, blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	// If the result contains a revert reason, try to unpack and return it.
-	if len(resultBefore.Revert()) > 0 {
-		return nil, newRevertError(resultBefore)
-	}
-	return resultBefore.Return(), resultBefore.Err
-}
-// func (s *PublicTransactionPoolAPI) DoSimulate(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes, error) {
+// func DoSimulate(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride, s *PublicTransactionPoolAPI) (hexutil.Bytes, error) {
 // 	resultBefore, err := DoCallForTest(ctx, s.b, args, args0, blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
 // 	if err != nil {
 // 		fmt.Println(err)
@@ -1796,6 +1784,18 @@ func DoSimulate(ctx context.Context, args TransactionArgs, args0 TransactionArgs
 // 	}
 // 	return resultBefore.Return(), resultBefore.Err
 // }
+func (s *PublicTransactionPoolAPI) DoSimulate(ctx context.Context, args TransactionArgs, args0 TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes, error) {
+	resultBefore, err := DoCallForTest(ctx, s.b, args, args0, blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	// If the result contains a revert reason, try to unpack and return it.
+	if len(resultBefore.Revert()) > 0 {
+		return nil, newRevertError(resultBefore)
+	}
+	return resultBefore.Return(), resultBefore.Err
+}
 
 
 
